669cb3a3dfc293d48482017aa20958fa53311a4c
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-10-03 19:32:51 +0300

    refactor: auth flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Res } from '@nestjs/common';
-import { type Response } from 'express';
-import { AuthService } from './auth.service';
-import { type UserDTO } from '../modules/users/DTO/user.dto';
-import { Public } from './decorators/auth.decorators';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Res,
+  Req,
+  HttpCode,
+  BadRequestException,
+} from '@nestjs/common'
+import { type Response, type Request } from 'express'
+import { AuthService } from './auth.service'
+import { Public } from './decorators/auth.decorators'
+import { RegisterDTO } from './dto/reginster.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -13,27 +22,72 @@ export class AuthController {
     return {
       status: 'ok',
       message: 'Auth module is working',
-    };
+    }
   }
 
   @Public()
   @Post('register')
   async register(
-    @Body() userDto: UserDTO,
+    @Body() userDto: RegisterDTO,
     @Res({ passthrough: true }) response: Response,
   ) {
-    const user = await this.authService.register(userDto);
+    const user = await this.authService.register(userDto)
     response.cookie('refreshToken', user.refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      path: 'api/v1/auth',
-    });
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //TODO: replace with env expiration time
+      path: '/api/v1/auth',
+    })
     return {
       accessToken: user.accessToken,
       id: user.id,
       email: user.email,
-    };
+    }
+  }
+
+  @Public()
+  @Post('login')
+  async login(
+    @Body() userDto: RegisterDTO,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const user = await this.authService.login(userDto)
+    if (!user)
+      return response.status(403).json({ message: 'Invalid email or password' })
+    response.cookie('refreshToken', user.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //TODO: replace with env expiration time
+      path: '/api/v1/auth',
+    })
+    return {
+      accessToken: user.accessToken,
+      id: user.id,
+      email: user.email,
+    }
+  }
+
+  @Public()
+  @Post('logout')
+  @HttpCode(200)
+  async logout(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const refreshToken = request.cookies?.refreshToken as unknown as string
+
+    if (!refreshToken) {
+      throw new BadRequestException('Refresh token not found')
+    }
+
+    await this.authService.logout({ refreshToken })
+
+    response.clearCookie('refreshToken', {
+      path: '/api/v1/auth',
+    })
+
+    return { message: 'Logged out successfully' }
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { UsersModule } from '../modules/users/users.module';
-import { TokenService } from './services/token.service';
-import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { JwtService } from '@nestjs/jwt';
+import { Module } from '@nestjs/common'
+import { AuthController } from './auth.controller'
+import { UsersModule } from '../modules/users/users.module'
+import { TokenService } from './services/token.service'
+import { AuthService } from './auth.service'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { JwtService } from '@nestjs/jwt'
+import { SessionsModule } from '../modules/sessions/sessions.module'
 
 @Module({
   controllers: [AuthController],
-  imports: [UsersModule],
+  imports: [UsersModule, SessionsModule],
   providers: [TokenService, AuthService, JwtStrategy, JwtService],
   exports: [AuthService],
 })
