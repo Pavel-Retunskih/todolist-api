2560b79b179e3af6f6d119180c818f656a6c7eea
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-10-03 19:36:25 +0300

    chore: add cookies parser

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   // Устанавливаем глобальный префикс для всех API маршрутов
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1')
+
+  // Подключаем парсинг cookies для доступа к request.cookies
+  app.use(cookieParser());
 
   // Получаем Reflector из контекста приложения
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 5000)
 }
 
 bootstrap().then(() =>
   console.log(
     `🚀 Application is running on: http://localhost:${process.env.PORT ?? 5000}/api/v1`,
   ),
-);
+)
