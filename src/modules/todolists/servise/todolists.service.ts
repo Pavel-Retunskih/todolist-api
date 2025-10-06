1e1c3154dec6e40a6b939f6e653931daece2cc23
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-10-06 17:00:35 +0300

    feat: add todolist service

---
+++ src/modules/todolists/servise/todolists.service.ts
@@ -0,0 +1,61 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { type TodolistRepository } from '../domain/todolist.repository'
+import { TodolistEntity } from '../domain/todolist.entity'
+import { CreateTodoDTO, UpdateTodoDTO } from '../dto/CreateTodoDTO'
+import { ConfigService } from '@nestjs/config'
+import { TooManyTodolistsException } from '../domain/too-many-todolists-error.domain'
+
+@Injectable()
+export class TodolistsService {
+  constructor(
+    @Inject('TodolistsRepository')
+    private readonly todoistsRepository: TodolistRepository,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async createTododlist(
+    { title, description, imageUrl }: CreateTodoDTO,
+    ownerId: string,
+  ): Promise<TodolistEntity> {
+    const todolistsCount =
+      await this.todoistsRepository.getTodolistsCountByOwnerId(ownerId)
+    if (
+      todolistsCount >=
+      this.configService.getOrThrow<number>('todolists.itemsPerPage')
+    ) {
+      throw new TooManyTodolistsException()
+    }
+    return await this.todoistsRepository.createTodolist({
+      ownerId,
+      title,
+      description: description ? description : null,
+      imageUrl: imageUrl ? imageUrl : null,
+    })
+  }
+
+  async updateTodolist(
+    patchData: UpdateTodoDTO,
+    todoId: string,
+  ): Promise<TodolistEntity> {
+    const updatedTodolist = await this.todoistsRepository.updateTodolist(
+      todoId,
+      patchData,
+    )
+    if (!updatedTodolist) {
+      throw new Error('Todolist not found')
+    }
+    return updatedTodolist
+  }
+
+  async deleteTodolist(todoId: string): Promise<TodolistEntity | null> {
+    return await this.todoistsRepository.deleteTodolist(todoId)
+  }
+
+  async getTodolistById(todoId: string): Promise<TodolistEntity | null> {
+    return await this.todoistsRepository.getTodolistById(todoId)
+  }
+
+  async getAllByOwnerId(ownerId: string): Promise<TodolistEntity[]> {
+    return await this.todoistsRepository.getAllByOwnerId(ownerId)
+  }
+}
